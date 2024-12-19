c841cf65b6ef292b849bb088c9afc0e7f876cd20
Saurabh <saurabhnishad@hotmail.com>
2024-12-19 17:20:31 +0530

    inspect

--- DSA a2z/src/pattern/PatternPract.java
+++ DSA a2z/src/pattern/PatternPract.java
@@ -126,6 +126,16 @@ public class PatternPract {
         }
     }
 
+    static void rowCount1(int n) {
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
     static void increaseCount(int n) {
         for (int i = 1; i < n; i++) {
             for (int j = 1; j <= i; j++) {
